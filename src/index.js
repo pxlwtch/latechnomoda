bdee8891269b9b9f5db24520dcdf5cf08e1bdaf9
Kevin Hernandez <kevin.hernandez@slalom.com>
2017-04-29 22:28:21 -0700

    feature(webpack): Webpack now bundles JS and Sass
    Right after a git pull, run `npm i` to install `node_modules/`
    Then as you work in dev mode, type `npm run build` and see the bundles
    in the `dist/` folder

---
+++ src/index.js
@@ -0,0 +1,6 @@
+require('./static/sass/bandshite.scss');
+require('./static/sass/bio.scss');
+require('./static/sass/styles.scss');
+require('./static/sass/work.scss');
+
+console.log('hi');
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin("bundle.css");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+    // publicPath: "/dist"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({filename: 'index.html', template: 'src/index.html'}),
+    new HtmlWebpackPlugin({filename: 'main.html', template: 'src/main.html'}),
+    new HtmlWebpackPlugin({filename: 'bio.html', template: 'src/bio.html'}),
+    new HtmlWebpackPlugin({filename: 'resume.html', template: 'src/resume.html'}),
+    new HtmlWebpackPlugin({filename: 'work.html', template: 'src/work.html'}),
+    new HtmlWebpackPlugin({filename: 'beverly.html', template: 'src/case-study/beverly.html'}),
+    new HtmlWebpackPlugin({filename: 'bandshite.html', template: 'src/case-study/bandshite.html'}),
+    extractSass,
+    extractSass
+  ],
+  module: {
+    rules: [
+      // { test: /\.html$/, use: [ 'file-loader?name=[path][name].[ext]!extract-loader!html-loader' ] }
+      {test: /\.jpg$/, use: ["file-loader"]},
+      {test: /\.png$/, use: ["url-loader?mimetype=image/png"]},
+      // {test: /\.scss$/, use: [ "css-loader", "sass-loader" ]},
+      {test: /\.scss$/, use: extractSass.extract({
+        use: ["css-loader", "sass-loader"],
+        fallback: "style-loader",
+        publicPath: "static/css"
+      })},
+      {test: /\.html$/, use: [{loader: 'html-loader', options: { minimize: true }}]}
+    ]
+  }
+};
