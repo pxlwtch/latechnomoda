fd23dc5dcdfd21a90a1b3ac762b8d13338198f66
kevdez <contact@whatsnextforkev.in>
2017-05-07 15:51:35 -0700

    Finished beverly case study

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('./static/sass/styles.scss');
 require('./static/sass/case-study.scss');
 require('./static/sass/bio.scss');
 require('./static/sass/bandshite.scss');
+require('./static/sass/beverly.scss');
 //require('./static/sass/work.scss');
 
 console.log('hi this site was made by ...');
