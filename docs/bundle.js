fcdae76f5d77929d75ff4ddc670f5fe141357d52
kevdez <contact@whatsnextforkev.in>
2017-05-07 18:22:52 -0700

    Near complete with finishing case-study sass

--- docs/bundle.js
+++ docs/bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -92,12 +92,33 @@
 
 /***/ }),
 /* 4 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(3);
-__webpack_require__(2);
-__webpack_require__(1);
 __webpack_require__(0);
+__webpack_require__(6);
+__webpack_require__(4);
+__webpack_require__(3);
+__webpack_require__(1);
+__webpack_require__(2);
+__webpack_require__(5);
 //require('./static/sass/work.scss');
 
 console.log('hi this site was made by ...');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require('./static/sass/aqua.scss');
 require('./static/sass/styles.scss');
 require('./static/sass/case-study.scss');
 require('./static/sass/bio.scss');
 require('./static/sass/bandshite.scss');
 require('./static/sass/beverly.scss');
+require('./static/sass/rosie.scss');
 //require('./static/sass/work.scss');
 
 console.log('hi this site was made by ...');
