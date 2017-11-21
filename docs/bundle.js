31ff90c3b83cac4ef48a2ae5c02d2b7bbd6a4c98
kevdez <contact@whatsnextforkev.in>
2017-11-20 22:03:34 -0800

    Changed feature tiles and added favicon

--- docs/bundle.js
+++ docs/bundle.js
@@ -11242,38 +11242,47 @@ __webpack_require__(1);
 __webpack_require__(2);
 __webpack_require__(4);
 
-console.log('hi this site was made with ❤︎ by @daliafyi and @0XDEAD37 on twitter');
+console.log(
+  'hi this site was made with ❤︎ by @daliafyi and @0XDEAD37 on twitter'
+);
 
-
-$('.subtitle').on('click', function(){
-  console.log("you clicked on subtitle");
-})
+$('.subtitle').on('click', function() {
+  console.log('you clicked on subtitle');
+});
 $('a').on('click', function(event) {
-  if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
+  if (this.hash !== '') {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate(
+      {
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      },
+      800,
+      function() {
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
-      });
+      }
+    );
   }
 });
 
-window.sr = scrollreveal({reset:true, viewFactor: 0.25, duration: 1000});
-window.sr.reveal('.hi', {delay:500, duration: 1000});
-window.sr.reveal('.subtitle', {delay:1000, duration: 1500, easing: 'ease'});
+window.sr = scrollreveal({ reset: true, viewFactor: 0.25, duration: 1000 });
+window.sr.reveal('.hi', { delay: 500, duration: 1000 });
+window.sr.reveal('.subtitle', { delay: 1000, duration: 1500, easing: 'ease' });
 window.sr.reveal('.section-title');
 window.sr.reveal('.holo-bar');
 window.sr.reveal('.navigation-bar');
-window.sr.reveal('.work-piece');
+window.sr.reveal('.work-piece', {
+  delay: 1000,
+  duration: 1500,
+  easing: 'ease'
+});
 window.sr.reveal('.header');
-window.sr.reveal('.bio-content');
+window.sr.reveal('.about-paragraph');
 window.sr.reveal('.bottom-links');
 window.sr.reveal('.blue-text');
 window.sr.reveal('.blue-line');
